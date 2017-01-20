048256eecee4c073b63587168317cca5a1531670
otofune <otofune@gmail.com>
2017-01-20 13:09:40 +0900

    gulpfile: load configuration correctly

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@
 const ts = require('typescript');
 const fs = require('fs');
 const code = ts.transpile(fs.readFileSync('./gulpfile.ts').toString());
-const config = ts.transpile(fs.readFileSync('./src/config.ts').toString());
+const config = eval(ts.transpile(fs.readFileSync('./src/config.ts').toString().replace(/<.+>/,'')));
 eval(code);
