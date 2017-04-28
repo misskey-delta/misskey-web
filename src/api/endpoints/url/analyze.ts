ad93f8a42f65200d679ce947cc629425d8ce4717
otofune <otofune@gmail.com>
2017-04-28 16:02:37 +0900

    [api:url/analyze] use summaly plugin
    close #63.

--- src/api/endpoints/url/analyze.ts
+++ src/api/endpoints/url/analyze.ts
@@ -3,6 +3,7 @@ import * as URL from 'url';
 import * as request from 'request';
 import * as mongoose from 'mongoose';
 import tracer from 'trace-redirect';
+import summaly from 'summaly';
 const jade: any = require('jade');
 
 const client: any = require('cheerio-httpcli');
@@ -57,12 +58,6 @@ async function analyzer(url: URL.Url): Promise<[Date, string]> {
 	// call custom analyzers
 	const custom = async (crawlURL: URL.Url) => {
 		switch (crawlURL.hostname) {
-			case 'ja.wikipedia.org':
-			case 'en.wikipedia.org':
-				return await analyzeWikipedia(crawlURL);
-			case 'ja.m.wikipedia.org':
-			case 'en.m.wikipedia.org':
-				return await analyzeMobileWikipedia(crawlURL);
 			case 'gyazo.com':
 				return analyzeGyazo(crawlURL);
 			case 'yabumi.cc':
@@ -115,67 +110,6 @@ function showImage(src: string, href: string): string {
 	});
 
 	return image;
-}
-
-async function analyzeWikipedia(url: URL.Url): Promise<string> {
-	const title: string = decodeURI(url.pathname.split('/')[2]);
-
-	const result = await client.fetch(url.href);
-
-	if (result.error !== undefined && result.error !== null) {
-		throw new Error("crawl-error");
-	}
-
-	const $: any = result.$;
-
-	const text: string = $('#mw-content-text > p:first-of-type').text();
-
-	// Favicon
-	const icon: string = URL.resolve(url.href, $('link[rel="shortcut icon"]').attr('href'));
-
-	const compiler: (locals: any) => string = jade.compileFile(
-		`${__dirname}/summary.jade`);
-
-	const viewer = compiler({
-		url: url,
-		title,
-		icon,
-		description: text,
-		image: 'https://ja.wikipedia.org/static/images/project-logos/enwiki.png',
-		siteName: 'Wikipedia'
-	});
-
-	return viewer;
-}
-
-async function analyzeMobileWikipedia(url: URL.Url): Promise<string> {
-	const title: string = decodeURI(url.pathname.split('/')[2]);
-
-	const result = await client.fetch(url.href);
-	if (result.error !== undefined && result.error !== null) {
-		throw new Error("crawl-error");
-	}
-
-	const $: any = result.$;
-
-	const text: string = $('#bodyContent > div:first-of-type > p:first-of-type').text();
-
-	// Favicon
-	const icon: string = URL.resolve(url.href, $('link[rel="shortcut icon"]').attr('href'));
-
-	const compiler: (locals: any) => string = jade.compileFile(
-		`${__dirname}/summary.jade`);
-
-	const viewer = compiler({
-		url: url,
-		title,
-		icon,
-		description: text,
-		image: 'https://ja.wikipedia.org/static/images/project-logos/enwiki.png',
-		siteName: 'Wikipedia'
-	});
-
-	return viewer;
 }
 
 function analyzeGyazo(url: URL.Url): string {
@@ -231,71 +165,7 @@ async function analyzeImgur(url: URL.Url): Promise<string> {
  * @param url url
  */
 async function analyzeGeneral(url: URL.Url): Promise<string> {
-	// リクエスト送信
-	const result = await client.fetch(url.href);
-
-	if (result.error !== undefined && result.error !== null) {
-		throw new Error("crawl-error");
-	}
-
-	// HTMLじゃなかった場合は中止
-	const contentType: string = result.response.headers['content-type'];
-	if (! contentType || contentType.indexOf('text/html') === -1) {
-		throw new Error("crawl-error-not-html");
-	}
-
-	const $: any = result.$;
-
-	let title = or(
-		$('meta[property="misskey:title"]').attr('content'),
-		$('meta[property="og:title"]').attr('content'),
-		$('meta[property="twitter:title"]').attr('content'),
-		$('title').text());
-	if (! title) {
-		throw new Error("crawl-error-empty-title");
-	}
-	title = clip(entities.decode(title), 100);
-
-	const lang: string = $('html').attr('lang');
-
-	const type = or(
-		$('meta[property="misskey:type"]').attr('content'),
-		$('meta[property="og:type"]').attr('content'));
-
-	let image = or(
-		$('meta[property="misskey:image"]').attr('content'),
-		$('meta[property="og:image"]').attr('content'),
-		$('meta[property="twitter:image"]').attr('content'),
-		$('link[rel="image_src"]').attr('href'),
-		$('link[rel="apple-touch-icon"]').attr('href'),
-		$('link[rel="apple-touch-icon image_src"]').attr('href'));
-	image = image !== null ? wrapMisskeyProxy(URL.resolve(url.href, image)) : null;
-
-	let description = or(
-		$('meta[property="misskey:summary"]').attr('content'),
-		$('meta[property="og:description"]').attr('content'),
-		$('meta[property="twitter:description"]').attr('content'),
-		$('meta[name="description"]').attr('content'));
-	description = description !== null
-		? clip(entities.decode(description), 300)
-		: null;
-
-	if (title === description) {
-		description = null;
-	}
-
-	let siteName = or(
-		$('meta[property="misskey:site-name"]').attr('content'),
-		$('meta[property="og:site_name"]').attr('content'),
-		$('meta[name="application-name"]').attr('content'));
-	siteName = siteName !== null ? entities.decode(siteName) : null;
-
-	let icon = or(
-		$('meta[property="misskey:site-icon"]').attr('content'),
-		$('link[rel="shortcut icon"]').attr('href'),
-		$('link[rel="icon"]').attr('href'),
-		'/favicon.ico');
-	icon = icon !== null ? wrapMisskeyProxy(URL.resolve(url.href, icon)) : null;
+	const summary = await summaly(url.href)
 
 	const compiler: (locals: any) => string = jade.compileFile(
 		`${__dirname}/summary.jade`);
@@ -303,13 +173,11 @@ async function analyzeGeneral(url: URL.Url): Promise<string> {
 	// コンパイル
 	const viewer: string = compiler({
 		url: url,
-		title: title,
-		icon: icon,
-		lang: lang,
-		description: description,
-		type: type,
-		image: image,
-		siteName: siteName
+		title: summary.title,
+		icon: summary.icon,
+		description: summary.description,
+		image: summary.thumbnail,
+		siteName: summary.sitename
 	});
 
 	return viewer;
