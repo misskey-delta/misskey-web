f949ed325b53e9ae5a27b9e33f93ee01c7be459c
otofune <otofune@gmail.com>
2017-04-28 19:00:48 +0900

    [gulpfile] remove minimalize features
    - remove cssnano & uglify
    - generate src/sites/common/common.less with no base file
    - rollback browserify for peerdependency error by jadeify

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,172 +1,130 @@
-import { task, src, dest } from 'gulp';
+import * as gulp from 'gulp';
 import * as glob from 'glob';
 import * as ts from 'gulp-typescript';
 import * as tslint from 'gulp-tslint';
 import * as browserify from 'browserify';
-import {exec} from 'child_process';
+import * as Vinyl from 'vinyl';
 const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
 const es = require('event-stream');
 const less = require('gulp-less');
 const lessVars = require('gulp-less-json-variables');
-const cssnano = require('gulp-cssnano');
 const ls = require('gulp-livescript');
-const uglify = require('gulp-uglify');
 
 const tsProject = ts.createProject('tsconfig.json', {
 	typescript: require('typescript'),
 	target: 'ES5'
 });
 
-task('build', [
+gulp.task('build', [
 	'build:ts',
-	'copy:bower_components',
 	'copy:client-templates',
 	'build:client-scripts',
 	'build:client-styles',
 	'build-copy'
 ]);
 
-task('build-develop', [
-	'build:ts',
-	'copy:bower_components',
-	'copy:client-templates',
-	'build-develop:client-scripts',
-	'build-develop:client-styles',
-	'build-develop-copy'
-]);
-
-task('build:ts', () => {
+gulp.task('build:ts', () => {
 	return tsProject.src()
 		.pipe(tsProject())
-		.pipe(dest('./built'));
+		.pipe(gulp.dest('./built'));
 });
 
-task('copy:bower_components', () => {
-	return src('./bower_components/**/*')
-		.pipe(dest('./built/resources/bower_components'));
-});
-
-task('compile:client-scripts', ['build:ts'], () => {
+gulp.task('compile:client-scripts', ['build:ts'], () => {
 	return es.merge(
-		src('./src/sites/**/*.ls')
+		gulp.src('./src/sites/**/*.ls')
 			.pipe(ls()),
-		src('./built/sites/common/scripts/*.js')
-			.pipe(dest('./tmp/common/scripts/')),
-		src('./src/sites/common/scripts/*.js')
-			.pipe(dest('./tmp/common/scripts/')),
-		src([
+		gulp.src('./built/sites/common/scripts/*.js')
+			.pipe(gulp.dest('./tmp/common/scripts/')),
+		gulp.src('./src/sites/common/scripts/*.js')
+			.pipe(gulp.dest('./tmp/common/scripts/')),
+		gulp.src([
 			'./src/sites/*/common/**/*.js',
 			'./src/sites/*/pages/**/*.js',
 			'!./src/sites/*/pages/**/controller.js'])
-	).pipe(dest('./tmp/'));
+	).pipe(gulp.dest('./tmp/'));
 });
 
-task('copy:client-templates', () => {
+gulp.task('copy:client-templates', () => {
 	return es.merge(
-		src('./src/sites/**/common/views/**/*.jade')
-			.pipe(dest('./tmp/')),
-		src('./src/sites/**/pages/**/*.jade')
-			.pipe(dest('./tmp/')),
-		src('./src/sites/**/common/widgets/**/*.jade')
-			.pipe(dest('./tmp/')),
-		src('./src/sites/common/**/*.jade')
-			.pipe(dest('./tmp/common'))
+		gulp.src('./src/sites/**/common/views/**/*.jade')
+			.pipe(gulp.dest('./tmp/')),
+		gulp.src('./src/sites/**/pages/**/*.jade')
+			.pipe(gulp.dest('./tmp/')),
+		gulp.src('./src/sites/**/common/widgets/**/*.jade')
+			.pipe(gulp.dest('./tmp/')),
+		gulp.src('./src/sites/common/**/*.jade')
+			.pipe(gulp.dest('./tmp/common'))
 	);
 });
 
-task('build:client-scripts', ['copy:client-templates', 'compile:client-scripts'], done => {
+gulp.task('build:client-scripts', ['copy:client-templates', 'compile:client-scripts'], done => {
 	glob('./tmp/**/*.js', (err: Error, files: string[]) => {
 		const tasks = files.map((entry: string) => {
 			return browserify({ entries: [entry] })
 				.bundle()
 				.pipe(source(entry.replace('tmp', 'resources')))
-				.pipe(buffer())
-				.pipe(uglify())
-				.pipe(dest('./built'));
+				.pipe(gulp.dest('./built'));
 		});
-		src('./tmp/common/scripts/*.js')
-			.pipe(dest('./built/sites/common/scripts/'));
+		gulp.src('./tmp/common/scripts/*.js')
+			.pipe(gulp.dest('./built/sites/common/scripts/'));
 		es.merge(tasks).on('end', done);
 	});
 });
 
-task('build-develop:client-scripts', ['copy:client-templates', 'compile:client-scripts'], done => {
-	glob('./tmp/**/*.js', (err: Error, files: string[]) => {
-		const tasks = files.map((entry: string) => {
-			return browserify({ entries: [entry] })
-				.bundle()
-				.pipe(source(entry.replace('tmp', 'resources')))
-				.pipe(dest('./built'));
-		});
-		src('./tmp/common/scripts/*.js')
-			.pipe(dest('./built/sites/common/scripts/'));
-		es.merge(tasks).on('end', done);
+gulp.task('create:common-less', () => {
+	const theme = {
+		'@theme-color': config.publicConfig.themeColor,
+		'@resources-url': `"${config.publicConfig.resourcesUrl}"`
+	};
+	const vinyl = new Vinyl({
+		path: 'common.less',
+		contents: new Buffer('\n')
 	});
+	return es.readArray([vinyl]).pipe(lessVars(theme)).pipe(gulp.dest('./src/sites/common/'));
 });
 
-task('set-less-variables', () => {
-	exec('mkdir -p ./tmp/sites/common/')
-	exec('touch ./tmp/sites/common/common.less');
-	return src('./tmp/sites/common/common.less')
-		.pipe(lessVars({
-			'@theme-color': config.publicConfig.themeColor,
-			'@resources-url': "\"" + config.publicConfig.resourcesUrl + "\""
-		}))
-		.pipe(dest('./tmp/sites/common'));
-});
-
-task('build:client-styles', ['set-less-variables', 'copy:bower_components'], () => {
-	return src('./src/sites/**/*.less')
+gulp.task('build:client-styles', ['create:common-less'],  () => {
+	return gulp.src('./src/sites/**/*.less')
 		.pipe(less())
-		.pipe(cssnano({
-			safe: true // 高度な圧縮は無効にする (一部デザインが不適切になる場合があるため)
-		}))
-		.pipe(dest('./built/resources'));
+		.pipe(gulp.dest('./built/resources'));
 });
 
-task('build-develop:client-styles', ['set-less-variables', 'copy:bower_components'], () => {
-	return src('./src/sites/**/*.less')
-		.pipe(less())
-		.pipe(dest('./built/resources'));
-});
-
-task('lint', () => {
-	return src(['./src/**/*.ts', './expansion-types/*.d.ts'])
+gulp.task('lint', () => {
+	return gulp.src(['./src/**/*.ts', './expansion-types/*.d.ts'])
 		.pipe(tslint({
 			formatter: "verbose"
 		}))
 		.pipe(tslint.report())
 });
 
-task('build-copy', ['build:client-scripts'], () => {
-	src(['./src/sites/*/common/**/*', './src/sites/*/pages/**/*'])
-		.pipe(dest('./built/resources'));
-	src([
+gulp.task('build-copy', ['build:client-scripts'], () => {
+	gulp.src(['./src/sites/*/common/**/*', './src/sites/*/pages/**/*'])
+		.pipe(gulp.dest('./built/resources'));
+	gulp.src([
 		'./src/**/*',
 		'!./src/**/*.ts',
 		'!./src/**/*.ls',
 		'!./src/**/*.js'
-	]).pipe(dest('./built'));
-	src([
+	]).pipe(gulp.dest('./built'));
+	gulp.src([
 		'./src/share/script.js'
-	]).pipe(dest('./built/share'));
-	src('./resources/**/*').pipe(dest('./built/resources/common/'));
+	]).pipe(gulp.dest('./built/share'));
+	gulp.src('./resources/**/*').pipe(gulp.dest('./built/resources/common/'));
 });
 
-task('build-develop-copy', ['build-develop:client-scripts'], () => {
-	src(['./src/sites/*/common/**/*', './src/sites/*/pages/**/*'])
-		.pipe(dest('./built/resources'));
-	src([
+gulp.task('build-develop-copy', ['build-develop:client-scripts'], () => {
+	gulp.src(['./src/sites/*/common/**/*', './src/sites/*/pages/**/*'])
+		.pipe(gulp.dest('./built/resources'));
+	gulp.src([
 		'./src/**/*',
 		'!./src/**/*.ts',
 		'!./src/**/*.ls',
 		'!./src/**/*.js'
-	]).pipe(dest('./built'));
-	src([
+	]).pipe(gulp.dest('./built'));
+	gulp.src([
 		'./src/share/script.js'
-	]).pipe(dest('./built/share'));
-	src('./resources/**/*').pipe(dest('./built/resources/common/'));
+	]).pipe(gulp.dest('./built/share'));
+	gulp.src('./resources/**/*').pipe(gulp.dest('./built/resources/common/'));
 });
 
-task('test', ['lint', 'build:ts']);
+gulp.task('test', ['lint', 'build:ts']);
