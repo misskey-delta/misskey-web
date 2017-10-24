1f03d5cc94e2b0b1496183e4b4d6adb2f8c8f2d9
otofune <otofune@gmail.com>
2017-10-24 22:31:31 +0900

    [streaming] APIのストリームを1ユーザー対1つしかリクエストしない

--- src/api/streaming/index.ts
+++ src/api/streaming/index.ts
@@ -11,7 +11,7 @@ const _MongoStore: MongoStore.MongoStoreFactory = MongoStore(express_session);
 import config from '../../config';
 import db from '../../db';
 import endpoints from './endpoints';
-import {logInfo} from 'log-cool';
+import { logInfo } from 'log-cool';
 
 interface IMessage {
 	type: string;
@@ -27,28 +27,38 @@ const sessionGetter = (sessionKey: string) => new Promise((res, rej) => {
 		if (err) {
 			rej(err);
 		}
-		if (! session) {
+		if (!session) {
 			rej(new Error('session is null'));
 		}
 		res(session);
 	});
 });
 
-const emitter = (socket: SocketIO.Socket, event: string, data: {[key: string]: string}, close?: boolean) => {
+const emitter = (socket: SocketIO.Socket, event: string, data: { [key: string]: string }, close?: boolean) => {
 	socket.emit(event, data);
 	if (close) {
 		socket.disconnect(true);
 	}
 };
 
+const connector = (endpoint: string): Promise<WebSocket.connection> => new Promise((resolve, reject) => {
+	const client = new WebSocket.client();
+	client.on('connectFailed', (error) => reject(error));
+	client.on('connect', (connection) => resolve(connection));
+	client.connect(endpoint);
+});
+
 export default (server: http.Server | https.Server): void => {
 	const io: SocketIO.Server = SocketIO.listen(server);
+
+	const connections: { [key: string]: WebSocket.connection } = {};
+	const connectedCounts: { [key: string]: number } = {};
 
 	endpoints.forEach(name => {
 		io.of(`/streaming/${name}`).on('connect', async (socket: SocketIO.Socket) => {
 			logInfo(`Request Socket.IO stream: /streaming/${name}`);
 			// クッキーが無い場合切断
-			if (! socket.handshake.headers.cookie) {
+			if (!socket.handshake.headers.cookie) {
 				emitter(socket, 'announcement', {
 					type: 'error',
 					message: `cookie doesn't exist`,
@@ -58,7 +68,7 @@ export default (server: http.Server | https.Server): void => {
 			}
 			const cookies: { [key: string]: string } = cookie.parse(socket.handshake.headers.cookie);
 			// cookieに必要情報が無い場合切断
-			if (! cookies[config.sessionKey] && ! /s:(.+?)\./.test(cookies[config.sessionKey])) {
+			if (!cookies[config.sessionKey] && ! /s:(.+?)\./.test(cookies[config.sessionKey])) {
 				emitter(socket, 'announcement', {
 					type: 'error',
 					message: `cookie ${config.sessionKey} doesn't exist`,
@@ -80,93 +90,115 @@ export default (server: http.Server | https.Server): void => {
 				return;
 			}
 
-			// APIのWebSocket
-			const client = new WebSocket.client();
-
-			// 接続できなかったら切断
-			client.on('connectFailed', (error) => {
-				emitter(socket, 'announcement', {
-					type: 'error',
-					message: `can't connection to upstream server`,
-					happen: 'proxy'
-				}, true);
-			});
-
-			client.on('connect', (upstream) => {
-				// upstream接続後にsocketの切断が合った場合にupstreamも正常に切断させる
-				socket.on('disconnect', (reason: string) => {
-					logInfo(`Diconnect Socket.IO stream: /streaming/${name} by ${reason}`);
-					upstream.close();
-				});
-
-				// なんらかのエラー
-				upstream.on('error', (error) => {
-					emitter(socket, 'announcement', {
-						type: 'error',
-						message: `can't establish connection`,
-						detail: error.toString(),
-						happen: 'upstream'
-					});
-				});
-
-				// 切断
-				upstream.on('close', (code, desc) => {
-					emitter(socket, 'announcement', {
-						type: 'error',
-						message: 'close by upstream',
-						detail: desc,
-						happen: 'upstream'
-					}, true);
-				});
-
-				// メッセージ受領 -> クライアントへ返答
-				upstream.on('message', (data) => {
-					// UTF-8 メッセージでなければ処理しない
-					if (! (data.type === 'utf8')) {
-						return;
-					}
-
-					let message: IMessage;
-					try {
-						message = JSON.parse(data.utf8Data);
-					} catch (e) {
-						// JSONじゃねえ、クールに去るぜ・・・
-						emitter(socket, 'announcement', {
-							type: 'error',
-							message: 'upstream returns non JSON data.',
-							happen: 'upstream'
-						}, true);
-						return;
-					}
-
-					// upstremのエラーメッセージ
-					if (message.type === 'error') {
-						emitter(socket, 'announcement', {
-							type: 'error',
-							message: 'upstream error happned',
-							detail: message.value.message,
-							happen: 'upstream'
-						}, true);
-						return;
-					}
-
-					// おりゃ
-					emitter(socket, message.type, message.value);
-				});
-			});
-
 			// 接続URL組み立て
 			const endpoint: string = (() => {
 				const base = `ws://${config.apiServerIp}:${config.apiServerPort}/streams/${name}?passkey=${config.apiPasskey}`;
 				switch (name) {
-					case 'talk':		return `${base}&user-id=${session.userId}&otherparty-id=${socket.handshake.query['otherparty-id']}`;
-					case 'group-talk':	return `${base}&user-id=${session.userId}&otherparty-id=${socket.handshake.query['group-id']}`;
-					default:			return `${base}&user-id=${session.userId}`;
+					case 'talk': return `${base}&user-id=${session.userId}&otherparty-id=${socket.handshake.query['otherparty-id']}`;
+					case 'group-talk': return `${base}&user-id=${session.userId}&otherparty-id=${socket.handshake.query['group-id']}`;
+					default: return `${base}&user-id=${session.userId}`;
 				}
 			})();
 
-			// APIに接続
-			client.connect(endpoint);
+			if (!(endpoint in connections)) {
+				try {
+					logInfo(`Connect upstream WebSocket stream: ${name} w/ user-id ${session.userId}`);
+					// APIに接続
+					connections[endpoint] = await connector(endpoint);
+					// クローズ時に connections[endpoint] を削除する
+					connections[endpoint].on('close', () => {
+						logInfo(`Close upstream WebSocket stream: ${name} w/ user-id ${session.userId}`);
+						delete connections[endpoint];
+					});
+				} catch (e) {
+					emitter(socket, 'announcement', {
+						type: 'error',
+						message: `can't establish connection`,
+						detail: e.toString(),
+						happen: 'upstream'
+					}, true);
+					throw e;
+				}
+			}
+			// connection に対してどれだけのクライアントがぶら下がっているかを記録
+			if (!(endpoint in connectedCounts)) {
+				connectedCounts[endpoint] = 0;
+			}
+			++connectedCounts[endpoint];
+
+			/**
+			 * ハンドラ生成・登録
+			 */
+			// なんらかのエラー
+			const upstreamErrorEventHandler = (error: Error) => {
+				emitter(socket, 'announcement', {
+					type: 'error',
+					message: `can't establish connection`,
+					detail: error.toString(),
+					happen: 'upstream'
+				});
+			};
+			// 切断時
+			const upstreamCloseEventHandler = (code: number, desc: string) => {
+				emitter(socket, 'announcement', {
+					type: 'error',
+					message: 'close by upstream',
+					detail: desc,
+					happen: 'upstream'
+				}, true);
+			};
+			// メッセージ受領 -> クライアントへ返答
+			const upstreamMessageEventHandler = (data: WebSocket.IMessage) => {
+				// UTF-8 メッセージでなければ処理しない
+				if (!(data.type === 'utf8')) {
+					return;
+				}
+
+				let message: IMessage;
+				try {
+					message = JSON.parse(data.utf8Data);
+				} catch (e) {
+					// JSONじゃねえ、クールに去るぜ・・・
+					emitter(socket, 'announcement', {
+						type: 'error',
+						message: 'upstream returns non JSON data.',
+						happen: 'upstream'
+					}, true);
+					return;
+				}
+
+				// upstremのエラーメッセージ
+				if (message.type === 'error') {
+					emitter(socket, 'announcement', {
+						type: 'error',
+						message: 'upstream error happned',
+						detail: message.value.message,
+						happen: 'upstream'
+					}, true);
+					return;
+				}
+
+				// おりゃ
+				emitter(socket, message.type, message.value);
+			};
+
+			// ハンドラー登録
+			connections[endpoint].on('error', upstreamErrorEventHandler);
+			connections[endpoint].on('close', upstreamCloseEventHandler);
+			connections[endpoint].on('message', upstreamMessageEventHandler);
+
+			// socketの切断が合った場合にupstreamがどこからも利用されていなかったら正常に切断させる
+			socket.on('disconnect', (reason: string) => {
+				logInfo(`Diconnect Socket.IO stream: /streaming/${name} by ${reason}`);
+				// ハンドラを削除
+				connections[endpoint].removeListener('error', upstreamErrorEventHandler);
+				connections[endpoint].removeListener('close', upstreamCloseEventHandler);
+				connections[endpoint].removeListener('message', upstreamMessageEventHandler);
+				// 誰からも利用されていないようならコネクションを削除
+				if (1 > --connectedCounts[endpoint]) {
+					connections[endpoint].close();
+				}
+			});
 		});
 	});
 };
