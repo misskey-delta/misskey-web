4115c23022ddc1732b9f3101e01e763ba459d808
otofune <otofune@gmail.com>
2017-11-21 22:51:50 +0900

    Revert "update(emoji): emojinize on client-side"
    This reverts commit 545934be0f15f8c838fd8deb87685ebad812aa35.

--- src/api/endpoints/posts/create-with-file.ts
+++ src/api/endpoints/posts/create-with-file.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import requestApi from '../../../core/request-api';
 
 export default function createWithFile(req: express.Request, res: express.Response): void {
+	const emojinize = require("@misskey/emojinize");
+
 	const file: Express.Multer.File = (<any>req).file;
 	if (file !== undefined && file !== null) {
 		const data: any = {};
@@ -28,7 +30,7 @@ export default function createWithFile(req: express.Request, res: express.Respon
 		const inReplyToPostId = req.body['in-reply-to-post-id'];
 		if (inReplyToPostId !== undefined && inReplyToPostId !== null && inReplyToPostId !== '') {
 			requestApi('posts/reply', {
-				'text': req.body.text,
+				'text': emojinize(req.body.text),
 				'files': fileEntity !== null ? fileEntity.id : null,
 				'in-reply-to-post-id': inReplyToPostId
 			}, req.user).then((post: Object) => {
@@ -38,7 +40,7 @@ export default function createWithFile(req: express.Request, res: express.Respon
 			});
 		} else {
 			requestApi('posts/create', {
-				'text': req.body.text,
+				'text': emojinize(req.body.text),
 				'files': fileEntity !== null ? fileEntity.id : null
 			}, req.user).then((post: Object) => {
 				res.send(post);
